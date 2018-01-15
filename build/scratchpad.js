67a2059bc1cb31805edb885f24377f0791b22d99
@evalDev <whatagit@developingdan.com>
2018-01-15 16:20:32 +0000

    gdr generates an array

--- build/scratchpad.js
+++ build/scratchpad.js
@@ -5,5 +5,20 @@ var habitTracker = new Vue({
   el: '#habitTracker',
   data: {
     message: 'Hello Vue!'
+  },
+  methods: {
+    generateDateRange: function generateDateRange() {
+      return [];
+    }
   }
+});
+
+// Tests
+
+describe('Habit Tracker', function () {
+  describe('generateDateRange', function () {
+    it('generates an array', function () {
+      expect(Array.isArray(habitTracker.generateDateRange())).toBe(true);
+    });
+  });
 });
--- src/scratchpad.js
+++ src/scratchpad.js
@@ -3,5 +3,18 @@ var habitTracker = new Vue({
   el: '#habitTracker',
   data: {
     message: 'Hello Vue!'
+  },
+  methods: {
+    generateDateRange: () => []
   }
+})
+
+// Tests
+
+describe('Habit Tracker', () => {
+  describe('generateDateRange', () => {
+    it('generates an array', () => {
+      expect(Array.isArray(habitTracker.generateDateRange())).toBe(true)
+    })
+  })
 })
